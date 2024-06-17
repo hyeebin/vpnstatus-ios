7c1f1d8943b54385e33753d50015f82fd44c7630
김혜빈 <developerhyebin@gmail.com>
2024-06-17 17:52:34 +0900

    Initial Commit

---
+++ VpnStatus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VpnStatus
+//
+//  Created by FuturetekICT on 6/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
